1e3b3ddea726d6dbd26ce5111f16c7ac3b8db0e5
RappelBerryPi <ryan.appel.333@gmail.com>
2022-04-10 18:50:59 -0500

    Updates to All General SPis. Addition of a keypairgeneratorspi

--- quantumssl/src/main/java/com/thatsdarlingmama/App.java
+++ quantumssl/src/main/java/com/thatsdarlingmama/App.java
@@ -4,27 +4,7 @@ import java.security.InvalidKeyException;
 import java.security.Key;
 import java.security.NoSuchAlgorithmException;
 import java.security.NoSuchProviderException;
-import java.security.PrivateKey;
-import java.security.Provider;
-import java.security.PublicKey;
-import java.security.Security;
-import java.security.Signature;
 import java.security.SignatureException;
-import java.util.Arrays;
-import java.util.Base64;
-
-import javax.crypto.KeyAgreement;
-
-import com.thatsdarlingmama.security.signature.Dilithium2SignatureSpi;
-import com.thatsdarlingmama.security.signature.Dilithium5SignatureSpi;
-import com.thatsdarlingmama.security.keyagreement.FireSaberKEMKeyAgreementSpi;
-import com.thatsdarlingmama.security.keyagreement.Kyber1024KeyAgreementSpi;
-import com.thatsdarlingmama.security.keyagreement.LightSaberKEMKeyAgreementSpi;
-import com.thatsdarlingmama.security.signature.RainbowIClassicSignatureSpi;
-import com.thatsdarlingmama.security.signature.picnicL1FSSignatureSpi;
-import com.thatsdarlingmama.security.signature.picnicL1fullSignatureSpi;
-
-import org.openquantumsafe.KEMs;
 
 /**
  * Hello world!
@@ -32,44 +12,49 @@ import org.openquantumsafe.KEMs;
  */
 public class App {
     public static void main(String[] args) throws SignatureException, InvalidKeyException, NoSuchAlgorithmException, NoSuchProviderException {
-        //Provider p = Security.getProvider("Dilithium");
-        //System.out.println(p.getInfo());
-
-
         /*
-        var spi = new picnicL1FSSignatureSpi();
+        String signatureType = "Dilithium5";
+        KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance(signatureType);
 
-        Signature signature = Signature.getInstance("picnic_L1_FS");
+        Signature signature = Signature.getInstance(signatureType);
+        Signature signature2 = Signature.getInstance(signatureType);
         
-        signature.initSign(spi.getPrivateKey());
+        signature.initSign(keyPairGenerator.generateKeyPair().getPrivate());
+        signature2.initSign(keyPairGenerator.generateKeyPair().getPrivate());
         signature.update("Hello World!".getBytes());
-        spi.update("Hello World!".getBytes());
+        signature2.update("Hello World!".getBytes());
         byte[] bytes = signature.sign();
-        byte[] bytes2 = spi.sign();
 
-        signature.initVerify(spi.getPublicKey());
-        System.out.println(signature.verify(bytes));
-        System.out.println(spi.verify(bytes2));
+        signature2.initVerify(keyPairGenerator.generateKeyPair().getPublic());
+        System.out.println(signature2.verify(bytes));
         */
 
+        /*
+        KeyPairGenerator keyAgreementGenerator = KeyPairGenerator.getInstance("Kyber1024");
+        KeyPairGenerator keyAgreementGenerator2 = KeyPairGenerator.getInstance("Kyber1024");
         KeyAgreement keyAgreement = KeyAgreement.getInstance("Kyber1024");
         KeyAgreement keyAgreement2 = KeyAgreement.getInstance("Kyber1024");
-        var spi = new Kyber1024KeyAgreementSpi();
-        var spi2 = new Kyber1024KeyAgreementSpi();
 
-        PrivateKey privateKey = spi.getPrivateKey();
-        PrivateKey privateKey2 = spi2.getPrivateKey();
-        PublicKey publicKey = spi.getPublicKey();
-        PublicKey publicKey2 = spi2.getPublicKey();
-
-        keyAgreement.init(privateKey);
-        keyAgreement2.init(privateKey2);
-        keyAgreement.doPhase(publicKey2, false);
-        keyAgreement2.doPhase(publicKey, false);
+        keyAgreement.init(keyAgreementGenerator.generateKeyPair().getPrivate());
+        keyAgreement2.init(keyAgreementGenerator2.generateKeyPair().getPrivate());
+        keyAgreement.doPhase(keyAgreementGenerator2.generateKeyPair().getPublic(), false);
+        keyAgreement2.doPhase(keyAgreementGenerator.generateKeyPair().getPublic(), false);
         byte[] cipherText  = keyAgreement.generateSecret();
         byte[] cipherText2 = keyAgreement2.generateSecret();
 
-        Key key = new Key() {
+        Key key = keyAgreement.doPhase(WrapBytes(cipherText2), true);
+        Key key2 = keyAgreement2.doPhase(WrapBytes(cipherText), true);
+
+        byte[] broken = Base64.getDecoder().decode(key.getFormat());
+        byte[] broken2 = Base64.getDecoder().decode(key2.getFormat());
+        System.out.println(Arrays.equals(broken2, key.getEncoded()));
+        System.out.println(Arrays.equals(broken, key2.getEncoded()));
+        */
+
+    }
+    private static Key WrapBytes(byte[] cipherText) {
+        return new Key() {
+
             @Override
             public String getAlgorithm() { return null; }
 
@@ -80,29 +65,7 @@ public class App {
 
             @Override
             public String getFormat() { return null; }
+            
         };
-        Key key2 = new Key() {
-            @Override
-            public String getAlgorithm() { return null; }
-
-            @Override
-            public byte[] getEncoded() {
-                return cipherText2;
-            }
-
-            @Override
-            public String getFormat() { return null; }
-        };
-
-
-
-        Key key3 = keyAgreement.doPhase(key2, true);
-        Key key4 = keyAgreement2.doPhase(key, true);
-
-        byte[] broken = Base64.getDecoder().decode(key3.getFormat());
-        byte[] broken2 = Base64.getDecoder().decode(key4.getFormat());
-
-        System.out.println("Same Key: " + Arrays.equals(broken, key4.getEncoded()));
-        System.out.println("Same Key: " + Arrays.equals(broken2, key3.getEncoded()));
     }
 }
